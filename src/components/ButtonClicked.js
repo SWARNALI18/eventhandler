6f616f50b347f341b102bfecad2c741f2c2f5410
Swarnali Bhattacharjee <60848680+SWARNALI18@users.noreply.github.com>
2021-05-11 22:18:49 +0530

    Update ButtonClicked.js

--- src/components/ButtonClicked.js
+++ src/components/ButtonClicked.js
@@ -26,7 +26,7 @@ class ButtonClicked extends Component {
             <div>
                 
                 
-                <div style={{backgroundColor: this.state.bgColor , height:'50%',width:'50%',padding:'10%',marginLeft:'15%',marginTop:'10%'}}>{this.state.bgColor}</div>
+                <div style={{backgroundColor: this.state.bgColor ,color:this.state.bgColor, height:'50%',width:'50%',padding:'10%',marginLeft:'15%',marginTop:'10%'}}>{this.state.bgColor}</div>
                 <button onClick={()=>this.eventHandler()} >Press</button>
                 <div>{this.state.message}</div>
             </div>
